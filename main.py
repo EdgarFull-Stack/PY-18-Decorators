912414395b89968f6114d54727a83699893899f4
Edgar Lipnevic <edgarlipnevic@gmail.com>
2025-02-11 20:54:29 +0200

    2 task add

--- main.py
+++ main.py
@@ -20,4 +20,71 @@ def i_didziasias(tekstas, funkcija=None):
     return tekstas.upper()
 print(i_didziasias('labas', apversti_teksta))
 print('-'*40)
+# Example
+class Darbuotojas:
+    def __init__(self, vardas, pavarde, pareigos):
+        self.vardas = vardas
+        self.pavarde = pavarde
+        self.pareigos = pareigos
+        self.__atlyginimas = None
 
+    def change_employee_name(self, vardas):
+        if not vardas:
+            raise ValueError('Name is not provided')
+        self.vardas = vardas
+
+    @staticmethod
+    def divide(a ,b):
+        if b == b:
+            raise ZeroDivisionError('Can not divide by zero')
+        return a / b
+
+    @classmethod
+    def super_constructor(cls, vardas, pavarde, pareigos):
+        return cls(vardas, pavarde, pareigos)
+
+    @property
+    def atlyginimas(self):
+        return self.__atlyginimas
+
+def registratorius(funkcija):
+    print(f'Funkcija: {funkcija}')
+    def apvalkalas(argumentas):
+        print(f'Argumentas: {argumentas}: ')
+        rezultatas = funkcija (argumentas)
+        if rezultatas % 2 == 0:
+            print(f'{rezultatas} yra lyginis')
+        else:
+            print(f'{rezultatas} yra nelyginis')
+        return rezultatas
+    return apvalkalas
+
+@registratorius
+def kvadratu(skaicius):
+    return skaicius ** 2
+print(kvadratu(8))
+print('-'*40)
+# Example
+import time
+def top_level_delay(seconds):
+    def returner_of_delayed_func(func):
+        def wrapper(*args, **kwargs):
+            print(f'Function work will start after:{seconds}')
+            time.sleep(seconds)
+            res = func(*args, **kwargs)
+            return res
+        return wrapper
+    return returner_of_delayed_func
+
+@top_level_delay(3)
+def grazink500():
+    return 500
+
+@top_level_delay(5)
+def vykdyk_aritmetika(skaicius1, skaicius2, veiksmas = None):
+    if veiksmas == 'atimtis':
+        return skaicius1 - skaicius2
+res = grazink500()
+print(res)
+res = vykdyk_aritmetika(100,1, 'atimtis')
+print(res)
--- tasks.py
+++ tasks.py
@@ -10,8 +10,33 @@ def apdoroti_teksta(tekstas,funkcija=None):
         tekstas = funkcija(tekstas)
     return tekstas.lower()
 print(apdoroti_teksta('Labas'))
-def keli_apdorojimai(tekstas, *funkcijos):
-    for func in funkcijos:
+def keli_apdorojimai(tekstas, *args):
+    for func in args:
         tekstas = func(tekstas)
     return tekstas
 print(keli_apdorojimai('LABAS', prideti_zenkliuka, apversti_teksta))
+print('-'*40)
+#  Task 2
+
+def sekimo_dekoratorius(funkcija):
+    def wrapper (*args, **kwargs):
+        print(f'Vykdoma funkcija {funkcija}')
+        res = funkcija(*args, **kwargs)
+        print(f'Funkcija baigta')
+        return res
+    return wrapper
+
+@sekimo_dekoratorius
+def vykdyk_aritmetika(skaicius1, skaicius2, veiksmas = None):
+    if veiksmas == 'dauginti':
+        return skaicius1 * skaicius2
+    if veiksmas == 'dalinti':
+        if  skaicius2 == 0:
+            raise ValueError ('Can not be divided by zero')
+        return skaicius1 / skaicius2
+
+print(vykdyk_aritmetika(3,4,'dauginti'))
+print(vykdyk_aritmetika(8,4,'dalinti'))
+print(vykdyk_aritmetika(1,2,'dalinti'))
+
+
